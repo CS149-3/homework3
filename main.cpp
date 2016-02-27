e93e1551e8fb18fc1b7950d044595b5cbdeaad9a
Johnathon Ludeman <john@ludeman.com>
2016-02-27 14:53:44 -0800

    removing some extra tabs

--- main.cpp
+++ main.cpp
@@ -79,10 +79,10 @@ int main(int argc, const char * argv[]) {
 	
 	for(int i = 0; i < 10; i++)
 	{
-			for(int j = 0; j < 10; j++)
-			{
-					cout << "row " << i << ",col " << j << "  "<< seats[i][j] << endl;
-			}
+		for(int j = 0; j < 10; j++)
+		{
+			cout << "row " << i << ",col " << j << "  "<< seats[i][j] << endl;
+		}
 	}
     return 0;
 }
